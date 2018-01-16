666617b5b8ab01e46463cd761a992dd9a878abc3
Ubuntu <ubuntu@ip-172-30-0-193.ap-southeast-2.compute.internal>
2018-01-16 06:07:21 +0000

    Added wallet list handling

--- src/transfer.cpp
+++ src/transfer.cpp
@@ -10,6 +10,9 @@ FILE *fp;
 int AddressCount;
 char AddressList[2000][100];
 int i;
+int WalletCount;
+char WalletList[2000][100];
+int j;
 
 
 int main()
@@ -22,17 +25,29 @@ int main()
     while (fgets( TmpStr,100,fp) > 0 ) {
         TmpStr[ strlen(TmpStr) -1] = '\0';
 	strcpy( AddressList[AddressCount++], TmpStr);
+        //printf("%d addresses loaded\n", AddressCount);
     }
+    fclose(fp);
+
+    fp = fopen("/home/ubuntu/monero_loadtest/src/wallets.txt", "r");
+    WalletCount = 0;
+
+    while (fgets( TmpStr,100,fp) > 0 ) {
+        TmpStr[ strlen(TmpStr) -1] = '\0';
+	strcpy( WalletList[WalletCount++], TmpStr);
+        //printf("%d wallets loaded\n", WalletCount);
+    }
+    fclose(fp);
 
     //printf("%d addresses loaded\n", AddressCount);
 
-    printf("A_lt177928652.bin\n");
+    printf("%s\n", WalletList[ rand() % WalletCount]);
     printf("testing123\n");
     usleep(100000);
     printf("save\n");
     usleep(100000);
     for (i=0;i<100;i++) {
-    	printf("transfer %s 0.0%d dd04bdf008f7601e024c323349c0a00aff1bf01bd0f2e38073142976bfb84762\n", AddressList[ rand() % AddressCount], rand() % 99999);
+    	printf("transfer %s 0.00%d dd04bdf008f7601e024c323349c0a00aff1bf01bd0f2e38073142976bfb84762\n", AddressList[ rand() % AddressCount], rand() % 99999);
         usleep(100000);
         printf("testing123\n");
         usleep(100000);
